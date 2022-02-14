214912adeb028d534b0762b16898ddc46db1ef51
MarynaVolkovnitska <mvolkovnickaja@gmail.com>
2022-02-14 13:54:25 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    $(document).ready(function () {
+        $('.carousel__inner').slick({
+            speed: 1200,
+            adaptiveHeight: true,
+            prevArrow: '<button type="button" class="slick-prev"><img src="icon/prev.png"/></button>',
+            nextArrow: '<button type="button" class="slick-next"><img src="icon/next.png"/></button>',
+            responsive: [
+                {
+                 breakpoint: 992,
+                    settings: {                    
+                    dots: true,
+                    arrows: false
+                }
+
+
+            }]
+        });
+    });
+
+
+});
