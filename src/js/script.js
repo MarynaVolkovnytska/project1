7c1dc3debd2c96161aa66a20fce5fc83820a196a
Maryna <mvolkovnickaja@gmail.com>
2022-02-16 14:28:14 +0200

    change_carousel

--- src/js/script.js
+++ src/js/script.js
@@ -4,8 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     $(document).ready(function () {
         $('.carousel__inner').slick({
-            speed: 1200,
-            adaptiveHeight: true,
+            speed: 1200,            
             prevArrow: '<button type="button" class="slick-prev"><img src="icon/prev.png"/></button>',
             nextArrow: '<button type="button" class="slick-next"><img src="icon/next.png"/></button>',
             responsive: [
